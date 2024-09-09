2206a0baec1d8a97f976631e79730776d1943f57
Adarsh Kadam <120251944+adarsh47-bug@users.noreply.github.com>
2024-09-10 01:48:19 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '',
+  baseURL: process.env.REACT_APP_API_URL || 'https://mybackend-alumnet.azurewebsites.net/',
 });
 
 export default instance;
